68b05fcd6e5022a9458cc2b7c73250dede8e7535
unknown <luongvu2605@gmail.com>
2022-04-25 11:21:20 +0700

    bai8thchanh2

---
+++ src/main/java/com/codegym/controller/PhoneController.java
@@ -0,0 +1,33 @@
+package com.codegym.controller;
+
+
+import com.codegym.model.PhoneNumber;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class PhoneController {
+    @GetMapping("/phone")
+    public String showForm(Model model){
+        model.addAttribute("phoneNumber",new PhoneNumber());
+        return "/home";
+    }
+    @PostMapping("/abc")
+    public String checkValidation(@Valid @ModelAttribute("phoneNumber") PhoneNumber phoneNumber, BindingResult bindingResult,Model model){
+        new PhoneNumber().validate(phoneNumber,bindingResult);
+        if (bindingResult.hasFieldErrors()){
+            return "/home";
+        }
+        else {
+            model.addAttribute("phoneNumber",phoneNumber);
+            return "/result";
+        }
+    }
+}
